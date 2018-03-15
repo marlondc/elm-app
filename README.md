b08018e09c280a87d465f6df2d454ab657b3d4d0
marlondc <mdatechong@gmail.com>
2018-03-15 10:17:23 -0400

    initial commit `multiple modules`

---
+++ README.md
@@ -0,0 +1,7 @@
+# Elm App
+
+Made using [this tutorial](https://www.elm-tutorial.org/en/04-starting/07-multiple-modules.html)
+
+### Notes
+
+module makes it exportable
---
+++ api.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+
+server.use(jsonServer.defaults());
+
+const router = jsonServer.router('db.json');
+
+server.use(router);
+
+console.log('listening at port 4000');
+
+server.listen(4000);
